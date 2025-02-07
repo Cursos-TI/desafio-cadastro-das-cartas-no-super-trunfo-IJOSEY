d572b5ec6c9125b037d717704d5bfc248a85e1ff
IJOSEY <ijoseybs@GMAIL.COM>
2025-02-07 13:51:29 +0000

    retirando linhas do basico pelo excesso de código.

--- intermediário.c
+++ intermediário.c
@@ -55,25 +55,7 @@ int main(){
     //calculo de atributo carta 2. 
     pibperc2 =  (float) pib2 /populacao2;
     densidade2 = (float) populacao2 / area2;
-
-    printf("Nome1: %s\n",nome);
-    printf("Código1: %d\n",codigo);
-    printf("Área1: %f\n",area);
-    printf("População1: %d\n",populacao);
-    printf("Número de pontos Turísticos1: %d\n",pontost);
-    printf("PIB1: %f\n",pib);
-    printf("Pib per capta: %2.f\n", pibperc1);
-    printf("Densidade Populacional: %2.f",densidade1);
-
-    printf("Nome2: %s\n",nome2); 
-    printf("Código2: %d\n",codigo2); 
-    printf("Área2: %2.ff\n",area2);
-    printf("População2: %d\n",populacao2);
-    printf("Número de pontos Turísticos2: %d\n",pontost2);
-    printf("PIB2: %2.f\n",pib2);
-    printf("Pib per capta: %2.f\n",pibperc2);
-    printf("Densidade Populacional: %2.f\n",densidade2);
-    
+        
     //Usuário deve determina se deja compara 1 ou 2 atributos 
     printf("1- para compara 1 atributo de cada carta.\n");
     printf("2- para compara 2 atributos de cada carta.\n");
@@ -181,7 +163,7 @@ int main(){
    case 'F':
    case 'f':
     printf("Você escolheu: F- Densidade Populacional.\n");
-    if (densidade1 > densidade2)
+    if (densidade1 < densidade2)
     {
        printf("1° Carta venceu!!\n");
     } 
